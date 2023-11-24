bb0ea5dc4dbe779871c9616ccd004d8f9875599f
Destructis <les.3e.anquetil@gmail.com>
2023-11-24 11:33:49 +0100

    adds

---
+++ application/Jeu.java
@@ -0,0 +1,8 @@
+package application;
+
+public class Jeu {
+    public void jouer() {
+        
+    }
+    
+}
--- modele/Personnage.java
+++ modele/Personnage.java
@@ -1,5 +1,21 @@
 package modele;
 
-public class Personnage{
-    
+public abstract class Personnage{
+    protected String nom;
+    protected int rang;
+    protected String caracteristiques;
+    protected Joueur joueur;
+    protected boolean assassine;
+    protected boolean vole;
+    protected Plateau plateau;
+
+    public Personnage(String nom, int rang, String caracteristiques) { //Constructeur
+        this.nom = nom;
+        this.rang = rang;
+        this.caracteristiques = caracteristiques;
+        joueur = null;
+        assassine = false;
+        vole = false;
+    }
+
 }
