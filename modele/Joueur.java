b85251250d43d1c579648667482c85acb187e301
Destructis <les.3e.anquetil@gmail.com>
2023-11-25 16:02:19 +0100

    fonction base pour joueur Co-authored-by: Fallemne <Fallemne@users.noreply.github.com> Co-authored-by: aigeax <aigeax@users.noreply.github.com> Co-authored-by: Sckryzer <Sckryzer@users.noreply.github.com>

--- modele/Joueur.java
+++ modele/Joueur.java
@@ -19,5 +19,20 @@ public class Joueur {
     public Quartier[] getCite() {}
     public ArrayList < Quartier > getMain() {}  
     public boolean isRangPlusEleve() {}
+    public void setRangPlusEleve() {}
+    public boolean isSimule() {}
+    public void setSimule(boolean simule) {}
+    public void setPremier(boolean premier) {}
+    public int nbQuartiersDansMain() {}
+    public boolean getPossedeCouronne() {}
+    public void setPossedeCouronne(boolean b) {}
+    public void ajouterPieces(int piece) {}
+    public void retirerPieces(int piece) {}
+    public void ajouterQuartierDansCite(Quartier quartier) {}
+    public boolean quartierPresentDansCite(String quartier) {}
+    public Quartier retirerQuartierDansCite(String quartier) {}
+    public void ajouterQuartierDansMain(Quartier quartier) {}
+    public Quartier retirerQuartierDansMain() {}
+    public void reinitialiser() {}
 }
   
