afc8658405b92977c4201e8230fe991ac566c440
Destructis <les.3e.anquetil@gmail.com>
2023-11-25 15:35:50 +0100

    Co-authored-by: Fallemne <Fallemne@users.noreply.github.com> Co-authored-by: aigeax <aigeax@users.noreply.github.com> Co-authored-by: Sckryzer <Sckryzer@users.noreply.github.com>

--- modele/Joueur.java
+++ modele/Joueur.java
@@ -3,16 +3,21 @@ package modele;
 import java.util.ArrayList;
 
 public class Joueur {
-    private String nom;
+    private String nom;   
     private int tresor;
     private Quartier[] cite;
     private int nbQuartiers;
     private ArrayList < Quartier > main;
-    private boolean possedeCouronne;
+    private boolean possedeCouronne;    
     
 
     public Joueur(String nom) {}
     public Personnage getPersonnage() {}
-    public String getNom() {}
+    public String getNom() {} 
     public int nbPieces() {}
+    public int nbQuartiersDansCite() {}
+    public Quartier[] getCite() {}
+    public ArrayList < Quartier > getMain() {}  
+    public boolean isRangPlusEleve() {}
 }
+  
