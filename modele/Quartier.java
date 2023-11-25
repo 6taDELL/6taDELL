17000e18dd29837e053e5d06d615926ac63e06ba
Destructis <les.3e.anquetil@gmail.com>
2023-11-25 14:28:50 +0100

    types quartier
    Co-authored-by: Fallemne <Fallemne@users.noreply.github.com>
    Co-authored-by: aigeax <aigeax@users.noreply.github.com>

--- modele/Quartier.java
+++ modele/Quartier.java
@@ -1,5 +1,15 @@
 package modele;
 
 public class Quartier {
-    
+    private String nom;
+    private String type;
+    private int coutConstruction;
+    private String caracteristiques;
+    public static final String[] TYPE_QUARTIERS = {
+        "RELIGIEUX",
+        "MILITAIRE",
+        "NOBLE",
+        "COMMERCANT",
+        "MERVEILLE"
+    };
 }
