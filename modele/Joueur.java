eabf6a04f7518e0bcdce8ed6c5fb56f3556e5d38
Destructis <les.3e.anquetil@gmail.com>
2023-11-25 14:59:56 +0100

    classe joueur

--- modele/Joueur.java
+++ modele/Joueur.java
@@ -1,4 +1,10 @@
 package modele;
 public class Joueur {
+    private nom: String;
+    private tresor: entier;
+    private cite: Quartier[];
+    private nbQuartiers: entier;
+    private main: ArrayList<Quartier>;
+    private possedeCouronne: boolean;
     
 }
