d53506e5f243d675d4f26521c647094d3f19cc2f
Destructis <les.3e.anquetil@gmail.com>
2023-11-30 19:24:56 +0100

    classe roi

---
+++ modele/Roi.java
@@ -0,0 +1,12 @@
+package modele;
+
+public class Roi extends Personnage{
+
+    public Roi(){
+        super("Roi",4, Caracteristiques.ROI);
+    }
+    public void utiliserPouvoir() {}
+    public void percevoirRessourcesSpecifiques() {}
+    
+
+}
