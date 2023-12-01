10218ac81ee9d3bd6ef7a4e42f1ec66f7ef43c7b
Fallemne <122923357+Fallemne@users.noreply.github.com>
2023-12-01 08:53:10 +0100

    classe roi

--- modele/Roi.java
+++ modele/Roi.java
@@ -1,12 +1,37 @@
 package modele;
 
-public class Roi extends Personnage{
+public class Roi extends Personnage {
 
-    public Roi(){
-        super("Roi",4, Caracteristiques.ROI);
+    public Roi() {
+        super("Roi", 4, Caracteristiques.ROI);
     }
-    public void utiliserPouvoir() {}
-    public void percevoirRessourcesSpecifiques() {}
-    
+
+
+    public void percevoirRessourcesSpecifiques() {
+        if (this.getJoueur() instanceof Joueur && !this.getAssassine()) {
+            int sumNoble = 0;
+            for (Quartier quartier: this.getJoueur().getCite()) {
+
+                if (quartier instanceof Quartier && quartier.getType() == Quartier.TYPE_QUARTIERS[2]) {
+                    sumNoble += 1;
+                }
+
+            }
+
+            this.getJoueur().ajouterPieces(sumNoble);
+            System.out.println(sumNoble + " pièces ajoutés.");
+        }
+    }
+
+    public void utiliserPouvoir() {
+        if (this.getJoueur() instanceof Joueur && !this.getAssassine()) {
+            this.getJoueur().setPossedeCouronne(true);
+            System.out.println("Je prends la couronne.");
+        }
+    }
+
+    public void utiliserPouvoirAvatar() {
+        this.utiliserPouvoir();
+    }
 
 }
