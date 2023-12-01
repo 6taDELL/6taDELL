10fe4a9dab8464f507ee5480fa39eb5355a3fd4a
Destructis <les.3e.anquetil@gmail.com>
2023-12-01 09:21:32 +0100

    classe assassin

---
+++ modele/Assassin.java
@@ -0,0 +1,11 @@
+package modele;
+
+import controlleur.Interaction;
+
+public class Assassin extends  Personnage{
+    public Assassin() {
+		super("Assassin", 1, Caracteristiques.ASSASSIN);
+	}
+    public void utiliserPouvoir() {}
+
+}
