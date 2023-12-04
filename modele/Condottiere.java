3c9e2e64d36e5bfc4eb3c9a2b8798e687601789b
Destructis <les.3e.anquetil@gmail.com>
2023-12-04 10:14:46 +0100

    classe condottiere

---
+++ modele/Condottiere.java
@@ -0,0 +1,19 @@
+package modele;
+
+import java.util.Random;
+
+import controlleur.Interaction;
+
+public class Condottiere extends Personnage {
+
+
+    public Condottiere() {
+        super("Condottiere", 8, Caracteristiques.CONDOTTIERE);
+
+    }
+
+    public void percevoirRessourcesSpecifiques() {}
+    public void utiliserPouvoir(){}
+    public void utiliserPouvoirAvatar(){}
+
+}
