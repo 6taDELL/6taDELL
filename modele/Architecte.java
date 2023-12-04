e6ffada0796a027ffce23733841a0eeb161c9860
Destructis <les.3e.anquetil@gmail.com>
2023-12-04 10:13:20 +0100

    classe architecte

---
+++ modele/Architecte.java
@@ -0,0 +1,11 @@
+package modele;
+
+public class Architecte extends Personnage {
+
+    public Architecte() {
+        super("Architecte", 6, Caracteristiques.ARCHITECTE);
+    }
+
+    public void utiliserPouvoir() {}
+    public void utiliserPouvoirAvatar(){}
+}
