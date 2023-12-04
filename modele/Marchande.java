4a448730816cc8cd028d2c41a595f82e05b68109
Destructis <les.3e.anquetil@gmail.com>
2023-12-04 10:12:06 +0100

    classe marchande

---
+++ modele/Marchande.java
@@ -0,0 +1,12 @@
+package modele;
+
+public class Marchande extends Personnage {
+    public Marchande() {
+        super("Marchande", 6, Caracteristiques.MARCHANDE);
+    }
+
+
+    public void utiliserPouvoir() {}
+    public void utiliserPouvoirAvatar(){}
+    
+}
