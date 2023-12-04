94897702ef12291ed029c3866f07a9eade5fd151
Destructis <les.3e.anquetil@gmail.com>
2023-12-04 10:03:14 +0100

    classe voleur

---
+++ modele/Voleur.java
@@ -0,0 +1,14 @@
+package modele;
+
+import controlleur.Interaction;
+import java.util.Random;
+
+public class Voleur extends Personnage {
+
+    public Voleur() {
+        super("Voleur", 2, Caracteristiques.VOLEUR);
+    }
+    public void utiliserPouvoir() {}
+    public void utiliserPouvoirAvatar() {}
+    
+}
