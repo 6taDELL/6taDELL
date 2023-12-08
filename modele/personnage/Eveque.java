b4f30467c88f44b4646ca65b315f1563abd042ee
Sckryzer <rom1.jaffre@gmail.com>
2023-12-08 19:05:22 +0100

    Eveque

--- modele/personnage/Eveque.java
+++ modele/personnage/Eveque.java
@@ -2,14 +2,25 @@ package modele.personnage;
 
 import modele.Caracteristiques;
 import modele.Personnage;
+import modele.Quartier;
 
 public class Eveque extends Personnage {
 
     public Eveque() {
         super("Eveque", 5, Caracteristiques.EVEQUE);
     }
-    public void utiliserPouvoir(){
+    public void utiliserPouvoir(){}
 
+    public void percevoirRessourcesSpecifiques() {
+        Quartier[] citeJ = this.getJoueur().getCite();
+        int nbQuartiers = this.getJoueur().nbQuartiersDansCite();
+
+        for (int i = 0; i < nbQuartiers; i++) {
+            if (citeJ[i] != null && citeJ[i].getType().equals(Quartier.TYPE_QUARTIERS[0])) {
+                getJoueur().ajouterPieces(1);
+            }
+        }
     }
+
     public void utiliserPouvoirAvatar(){}
 }
