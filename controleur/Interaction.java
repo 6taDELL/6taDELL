f2ac6f1b3d4f55f0ac83e61b126de2b8e0665aa0
Sckryzer <rom1.jaffre@gmail.com>
2023-12-12 18:04:18 +0100

    Fixing bugs

--- controleur/Interaction.java
+++ controleur/Interaction.java
@@ -2,7 +2,6 @@ package controleur;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
-import exception.*;
 
 public class Interaction {
 	private static Scanner sc = new Scanner(System.in);
@@ -18,32 +17,30 @@ public class Interaction {
 				System.out.print("Veuillez rentrer un chiffre : ");
 				sc.next(); // passe l'entier pour éviter de boucler
 			}
-		} while(continu);
+		} while (continu);
 		return i;
 	}
 
-	// renvoie un entier lu au clavier compris dans l'intervalle
-	//     [borneMin, borneMax[
-		public static int lireUnEntier(int borneMin, int borneMax) {
-			int i = 0;
-			boolean continu = true;
-			do {
-				try {
-					i = sc.nextInt();
-					if(i< borneMin || i>borneMax){
-						throw new OutOfBounds();
-					}
-					continu = false;
-				} catch (InputMismatchException e) {
-					System.out.print("Veuillez rentrer un entier : ");
-					sc.next();
-				} catch (OutOfBounds e){
-					System.out.println("Veuillez rentrer un entier compris entre " + borneMin + " et " + borneMax);
+	// renvoie un entier lu au clavier compris dans l'intervalle [borneMin, borneMax[
+	public static int lireUnEntier(int borneMin, int borneMax) {
+		int i = 0;
+		boolean isValidInput = false;
+		do {
+			try {
+				System.out.print("Veuillez entrer un entier entre " + borneMin + " et " + (borneMax - 1) + ": ");
+				i = sc.nextInt();
+				if (i >= borneMin && i < borneMax) {
+					isValidInput = true;
+				} else {
+					System.out.println("La valeur entrée n'est pas dans la plage spécifiée. Veuillez réessayer.");
 				}
-			} while(continu);
-			return i;
-		}
-		
+			} catch (InputMismatchException e) {
+				System.out.println("Veuillez entrer un entier valide.");
+				sc.next(); // Clear the invalid input
+			}
+		} while (!isValidInput);
+		return i;
+	}
 
 	// lit les réponses "oui", "non", "o" ou "n" et renvoie un booléen
 	public static boolean lireOuiOuNon() {
@@ -58,7 +55,4 @@ public class Interaction {
 		//...
 		return retour;
 	}
-
-
-	
 }
---
+++ out/production/6taDELL/ReadMe.md
@@ -0,0 +1,25 @@
+# Citadelle
+
+Le projet consiste à créer le jeu de societe "Citadelle" grâce au langage Java.
+
+---
+
+The project consists in creating the board game "Citadelle" using the Java language.
+
+# Equipe
+
+Nous sommes un groupe de 4 élèves au sein de l'Esaip:
+
+- ANQUETIL Eloi
+- BRUN-PICARD Matéo
+- JAFFRE Romain
+- SISSUNG Félix
+
+---
+
+We are a team with 4 students comming from ESAIP :
+
+- ANQUETIL Eloi
+- BRUN-PICARD Matéo
+- JAFFRE Romain
+- SISSUNG Félix
